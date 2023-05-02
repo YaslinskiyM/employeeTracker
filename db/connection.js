6132b22c04c88fa4908d88aca550a8d0bc202120
maria <mariayaslinskiy@gmail.com>
2023-05-02 06:50:38 -0700

    starting project

---
+++ db/connection.js
@@ -0,0 +1,10 @@
+// get the client
+const mysql = require('mysql2');
+
+// create the connection to database
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'password',
+  database: 'employee_tracker'
+});
